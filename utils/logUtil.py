b82251c9dde844adf2af9da8d69ef7aabe8ec4fb
philip <philip.xu.2022@gmail.com>
2023-12-05 16:17:42 +0800

    关掉无用log

--- utils/logUtil.py
+++ utils/logUtil.py
@@ -3,7 +3,6 @@ import Constants
 
 def print_debug(*args, **kwargs):
     if Constants.LOG_ENABLE:
-        print('99999999999999999999999999999999999999999')
         print(*args, **kwargs)
     else:
         pass
